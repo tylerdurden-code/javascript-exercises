b58672c2b59ce899acf71d9aa1a498c970ca9901
Tyler Durden <tylerdurden1412@gmail.com>
2022-02-14 17:42:57 +0200

    removeFromArray and sumAll DONE!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let sum = 0;
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR"
+    }
+    if (num1 > num2) {
+        for (i = num2; i < num1 + 1; i++) {
+            sum += i;
+        }
+
+        return sum;
+    }
+    for (i = num1; i < num2 + 1; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
