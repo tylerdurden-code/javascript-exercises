8065cbfb1e97ce484b140ee7fd43f907757a046f
Tyler Durden <tylerdurden1412@gmail.com>
2022-02-27 21:49:00 +0200

    solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a = []) {
+	let sum = 0;
+  for (i = 0;i < a.length; i++) {
+    sum += a[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a = []) {
+  let sum = 1;
+  for (i = 0;i < a.length; i++) {
+    sum *= a[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let sum = 1;
+  for (i = 0;i < b; i++) {
+    sum *= a
+  }
+  return sum
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) {
+    return 1;
+  }
+  else {
+    let sum = 1;
+    for (i = a; i > 0; i--) {
+      sum *= i;
+    }
+    return sum
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,33 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    text = text.toLowerCase();
+    text = text.split("");
+    console.log(text);
+    var newText = new Array();
+    for (i = 0; i < text.length; i++) {
+        if (alphabet.includes(text[i])) {
+            newText.push(text[i])
+        }
+    }
+    console.log(newText);
+    let newTextCopy = [];
+    for (z = 0; z < newText.length; z++) {
+        newTextCopy.push(newText[z]);
+    }
+    newTextReversed = newTextCopy.reverse();
+    console.log(newTextReversed)
+    newText = newText.join(' ')
+    newTextReversed = newTextReversed.join(' ');
+    console.log(newText)
+    console.log(newTextReversed)
+    if (newText === newTextReversed) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
