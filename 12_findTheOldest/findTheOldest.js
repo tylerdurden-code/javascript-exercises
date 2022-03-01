a8468ff09c1684f2675eda9974b1eb50546c140b
Tyler Durden <tylerdurden1412@gmail.com>
2022-03-01 12:47:40 +0200

    finished odins fundamentals 4

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,17 +1,27 @@
 const findTheOldest = function(list) {
-    for (i = 0;i < list.length; i++) {
-        let oldestName;
-        let oldestYear = 2022;
-        for (key in list[i]) {
-            if (key === "yearOfBirth") {
-                if (list[i][key] < oldestYear) {
-                    oldestYear = list[i][key]
-                    oldestName = list[i]['name']
-                }
+    let maxAge = list[1].yearOfDeath - list[1].yearOfBirth;
+    let oldestName = list[1].name;
+    let ite = 1;
+    for (i = 0; i < list.length; i++) {
+        if (list[i].yearOfDeath === undefined) {
+            let date = new Date();
+            let year = date.getFullYear()
+            let age = year - list[i].yearOfBirth
+            if (age > maxAge) {
+                maxAge = age;
+                oldestName = list[i].name;
+                ite = i;
             }
+            break;
+        }
+        let age = (list[i].yearOfDeath - list[i].yearOfBirth);
+        if (age > maxAge) {
+        maxAge = age;
+        oldestName = list[i].name;
+        ite = i;
         }
     }
-    return oldestName;
+    return list[ite];
 };
 
 // Do not edit below this line
